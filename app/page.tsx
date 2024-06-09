0f5f4258c849d98343eeba50346626578dce4e9a
jp3pe <rlaworhkd430430@gmail.com>
2024-06-09 16:38:46 +0900

    chore: Update page layout with max width for better responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Post from "@/app/ui/post";
 
 function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white max-w-[680px] mx-auto">
       <Post />
     </main>
   );
