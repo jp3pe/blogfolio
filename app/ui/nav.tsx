b4191e19b06dbc0087d1a1e8d97001c7a2bd2e2b
jp3pe <rlaworhkd430430@gmail.com>
2024-06-11 21:15:51 +0900

    chore: Add "Write" button to navigation bar

--- app/ui/nav.tsx
+++ app/ui/nav.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
+import { FaPen } from "react-icons/fa";
 
 export default function Nav() {
   return (
     <nav className="flex items-center justify-between p-4 bg-gray-800 text-white">
       <h1 className="text-2xl font-bold">Blogfolio</h1>
       <ul className="flex gap-4 items-center">
-        <li>Home</li>
+        <li>
+          <FaPen className="inline mr-2" />
+          Write
+        </li>
         <li>About</li>
         <li>Contact</li>
         <li className="relative h-10 w-10">
