8dc1c541ca32f3330d4e4b807a97fdcf4d813130
jp3pe <rlaworhkd430430@gmail.com>
2024-06-15 20:20:20 +0900

    refactor: Update database connection to use Next.js environment variables

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,11 +1,12 @@
 import mysql from 'mysql2/promise';
+import nextConfig from '@/next.config.mjs';
 
 export async function connectToDatabase() {
   const connection = await mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
+    host: nextConfig.env.MYSQL_HOST,
+    user: nextConfig.env.MYSQL_USER,
+    password: nextConfig.env.MYSQL_PASSWORD,
+    database: nextConfig.env.MYSQL_DATABASE,
   });
   return connection;
 }
