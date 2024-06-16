f772d85fc9fedf0eb63eb6f34d7b27bc7a3602ef
jp3pe <rlaworhkd430430@gmail.com>
2024-06-16 15:49:26 +0900

    function: Update ui from DB

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,7 +1,8 @@
-import mysql from 'mysql2/promise';
-import nextConfig from '@/next.config.mjs';
+import mysql, { FieldPacket } from "mysql2/promise";
+import nextConfig from "@/next.config.mjs";
+import { Post } from "@/app/lib/definitions";
 
-export async function connectToDatabase() {
+async function connectToDatabase() {
   const connection = await mysql.createConnection({
     host: nextConfig.env.MYSQL_HOST,
     user: nextConfig.env.MYSQL_USER,
@@ -9,4 +10,21 @@ export async function connectToDatabase() {
     database: nextConfig.env.MYSQL_DATABASE,
   });
   return connection;
+}
+
+export async function fetchAllPosts(): Promise<Post[]> {
+  const connection = await connectToDatabase();
+  const query = "SELECT * FROM post";
+  const [rows] = (await connection.execute(query)) as [any[], FieldPacket[]];
+  await connection.end();
+
+  const posts: Post[] = rows.map((row: Post) => ({
+    id: row.id,
+    title: row.title,
+    content: row.content,
+    created_at: new Date(row.created_at),
+    updated_at: new Date(row.updated_at),
+  }));
+
+  return posts;
 }
