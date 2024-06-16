41a7e2244f4597c230554fc6f87b5cec1c2e32c0
jp3pe <rlaworhkd430430@gmail.com>
2024-06-16 18:06:44 +0900

    function: Update html by markdown

--- app/ui/content.tsx
+++ app/ui/content.tsx
@@ -1,38 +1,12 @@
 import Image from "next/image";
 import H2 from "@/app/ui/h2";
-import P from "@/app/ui/p";
-import { fetchAllPosts } from "@/app/lib/db";
 
-export type H2Type = {
-  title: string;
-};
 
-export type PType = {
-  content: string;
-};
-
+// TODO: 필요한지 파악하기
 export default async function Content() {
-  const posts = await fetchAllPosts();
-
   return (
     <article className="flex flex-col">
-      {posts.map((post, index) => (
-        <div key={index}>
-          <H2 h2={{ title: post.title }} />
-          <Image
-            src="/image-sample.webp"
-            alt="sample image"
-            width={500}
-            height={500}
-            className="m-auto mb-5"
-          />
-          <P
-            p={{
-              content: post.content ?? "",
-            }}
-          ></P>
-        </div>
-      ))}
+
     </article>
   );
 }
--- app/ui/post.tsx
+++ app/ui/post.tsx
@@ -1,11 +1,45 @@
 import Content from "@/app/ui/content";
 import Header from "@/app/ui/header";
+import { Marked } from "@ts-stack/markdown";
+import DOMPurify from "dompurify";
+import { JSDOM } from "jsdom";
+import { fetchAllPosts } from "@/app/lib/db";
+import Image from "next/image";
 
-export default function Post() {
+// 마크다운을 안전한 HTML로 변환하는 함수
+const markdownToSafeHtml = (markdown: string): string => {
+  const dirtyHtml = Marked.parse(markdown);
+  const cleanHtml = DOMPurify(new JSDOM("<!DOCTYPE html>").window).sanitize(
+    dirtyHtml
+  );
+  return cleanHtml;
+};
+
+// 안전한 HTML을 렌더링하는 컴포넌트
+const SafeMarkdownComponent = ({ content }: { content: string }) => (
+  <div className="flex flex-col" dangerouslySetInnerHTML={{ __html: markdownToSafeHtml(content) }} />
+);
+
+export default async function Post() {
+  const posts = await fetchAllPosts();
+
   return (
     <main>
-      <Header />
-      <Content />
+      {posts.map((post) => (
+        <article key={post.id}>
+          <Header title={post.title} updated_at={post.updated_at} />
+          <Image
+            src="/image-sample.webp"
+            alt="sample image"
+            width={500}
+            height={500}
+            className="m-auto mb-5"
+          />
+          {/* <Content> */}
+          <SafeMarkdownComponent content={post.content ?? ""} />
+          {/* </Content> */}
+        </article>
+      ))}
     </main>
   );
 }
