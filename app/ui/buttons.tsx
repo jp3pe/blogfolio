5b481ab5b9a2f772f4a29fa87e81bd2f3e25da1d
jp3pe <rlaworhkd430430@gmail.com>
2024-06-26 08:59:23 +0900

    refactor: Move common className into common Button component

--- app/ui/buttons.tsx
+++ app/ui/buttons.tsx
@@ -1,14 +1,34 @@
 import { deletePost } from "@/app/lib/actions";
 import Link from "next/link";
 
+export function Button({
+  className,
+  type = "button",
+  children,
+}: {
+  className: string;
+  type?: "submit" | "reset" | "button";
+  children: React.ReactNode;
+}) {
+  const baseClass = "inline-flex justify-center rounded-md border border-transparent py-2 px-4 text-sm font-medium text-white shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2";
+  return (
+    <button type={type} className={`${baseClass} ${className}`}>
+      {children}
+    </button>
+  );
+}
+
 export function DeletePost({ id }: { id: string }) {
   const deletePostWithId = deletePost.bind(null, id);
 
   return (
     <form action={deletePostWithId}>
-      <button className="inline-flex justify-center rounded-md border border-transparent bg-red-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2">
+      <Button
+        className="bg-red-600 hover:bg-red-700 focus:ring-red-500"
+        type="submit"
+      >
         Delete
-      </button>
+      </Button>
     </form>
   );
 }
@@ -16,9 +36,9 @@ export function DeletePost({ id }: { id: string }) {
 export function UpdatePost({ id }: { id: string }) {
   return (
     <Link href={`/posts/put/${id}`}>
-      <button className="inline-flex justify-center rounded-md border border-transparent bg-gray-700 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2">
-        Modify
-      </button>
+        <Button className="bg-gray-700 hover:bg-gray-600 focus:ring-gray-500">
+          Modify
+        </Button>
     </Link>
   );
 }
@@ -31,11 +51,11 @@ export function SubmitPost({
   text: string;
 }) {
   return (
-    <button
+    <Button
       type={type}
-      className="inline-flex justify-center rounded-md border border-transparent bg-gray-700 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
+      className="bg-gray-700 hover:bg-gray-600 focus:ring-gray-500"
     >
       {text}
-    </button>
+    </Button>
   );
 }
