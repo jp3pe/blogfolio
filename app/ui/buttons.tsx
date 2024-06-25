885a0b26bcedc274daa883d1a093c6c20b80f779
jp3pe <rlaworhkd430430@gmail.com>
2024-06-26 08:48:50 +0900

    chore: Update submit button styling in buttons.tsx

--- app/ui/buttons.tsx
+++ app/ui/buttons.tsx
@@ -33,7 +33,7 @@ export function SubmitPost({
   return (
     <button
       type={type}
-      className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+      className="inline-flex justify-center rounded-md border border-transparent bg-gray-700 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
     >
       {text}
     </button>
