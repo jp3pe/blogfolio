146de3578ba1774f31334b80c49b45b2c694ddbd
jp3pe <rlaworhkd430430@gmail.com>
2024-06-30 13:15:33 +0900

    feat: Update form input fields styling

--- app/posts/put/[id]/page.tsx
+++ app/posts/put/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { updatePost } from "@/app/lib/actions";
 import { fetchPost } from "@/app/lib/db";
+import { SubmitPost } from "@/app/ui/buttons";
 import { notFound } from "next/navigation";
 
 export default async function Home({ params }: { params: { id: string } }) {
@@ -9,7 +10,6 @@ export default async function Home({ params }: { params: { id: string } }) {
   }
   const updatePostWithId = updatePost.bind(null, post?.id ?? "");
 
-  // TODO: value를 사용하기 때문에 사용자의 브라우저에서 <input> 태그에 대한 내용을 수정할 수 없는 문제 해결하기
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white max-w-[680px] mx-auto">
       <form action={updatePostWithId} className="w-full max-w-[501px]">
@@ -25,7 +25,7 @@ export default async function Home({ params }: { params: { id: string } }) {
             id="title"
             name="title"
             defaultValue={post?.title ?? ""}
-            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+            className="mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-lg h-12 px-4"
             required
           />
         </div>
@@ -40,17 +40,12 @@ export default async function Home({ params }: { params: { id: string } }) {
             id="content"
             name="content"
             defaultValue={post?.content ?? ""}
-            rows={4}
-            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+            rows={10}
+            className="mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm h-50 px-4 py-2"
             required
           ></textarea>
         </div>
-        <button
-          type="submit"
-          className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-        >
-          Submit
-        </button>
+        <SubmitPost type="submit" text="Submit" />
       </form>
     </main>
   );
